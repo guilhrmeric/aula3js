9726ac3e14a588b82add4552f7aa0de9a867850b
guilhrmeric <guiihricardo2006@gmail.com>
2024-12-09 22:26:01 -0300

    projeto java script aula3

---
+++ teste.js
@@ -0,0 +1,38 @@
+/*var a = 2 //number
+let b = "enzo" //string
+const ativo = true //boolean
+r = a + b
+if (ativo){
+console.log("aluno com acesso liberado!")
+}
+else {
+    console.log("aluno com acesso bloqueado!")
+
+}
+
+
+console.log("A soma de a (",a, ") + b (",b, ")  = ", r)*/
+
+
+
+var nome = "luana"
+// let sobrenome = " "
+
+function colocaSobrenome() {
+
+var sobrenome = "souza" //local
+console.log("Olá " + nome + " " + sobrenome )
+}
+
+function func1() {
+
+
+    console.log("Olá " + nome + " " + "sobrenome" )
+    }
+    colocaSobrenome()
+ func1()
+
+console.log("Olá " + nome + " " + sobrenome )
+
+
+
